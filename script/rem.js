1d6e328bc916512a04e1efd944a6371bdbe81b59
runman <Bill.Chen@shapejoy.com>
2018-02-21 00:00:39 +0800

    晚上首页

--- script/rem.js
+++ script/rem.js
@@ -5,8 +5,8 @@
         e = 1,
         f = 1 / e,
         g = "orientationchange" in window ? "orientationchange" : "resize",
-        h = b.createElement("meta");
-    h.name = "viewport", h.content = "width=device-width, user-scalable=no, initial-scale=" + f + ", maximum-scale=" + f + ", minimum-scale=" + f, c.firstElementChild.appendChild(h);
+        h = b.createElement("meta"); //把缩放比改为0.5在手机可以缩放
+    h.name = "viewport", h.content = "width=device-width, user-scalable=no, initial-scale=" + 0.5 + ", maximum-scale=" + 0.5 + ", minimum-scale=" + 0.5, c.firstElementChild.appendChild(h);
     var i = function() {
         var a = c.clientWidth;
         a / e > d && (a = d * e), c.dataset.width = a, c.dataset.persent = 100 * (a / d), c.style.fontSize = 100 * (a / d) + "px"
