9ee91c9a3d128eccbfb09785af114c0569d75f18
runman <Bill.Chen@shapejoy.com>
2018-02-25 21:54:36 +0800

    基本完成

--- script/rem.js
+++ script/rem.js
@@ -1,15 +1,12 @@
-! function(a) {
-    var b = window.document,
-        c = b.documentElement,
-        d = a ? ~~a.dataset.psd : 1920,
-        e = 1,
-        f = 1 / e,
-        g = "orientationchange" in window ? "orientationchange" : "resize",
-        h = b.createElement("meta"); //把缩放比改为0.5在手机可以缩放
-    h.name = "viewport", h.content = "width=device-width, user-scalable=no, initial-scale=" + 0.5 + ", maximum-scale=" + 0.5 + ", minimum-scale=" + 0.5, c.firstElementChild.appendChild(h);
-    var i = function() {
-        var a = c.clientWidth;
-        a / e > d && (a = d * e), c.dataset.width = a, c.dataset.persent = 100 * (a / d), c.style.fontSize = 100 * (a / d) + "px"
-    };
-    i(), b.addEventListener && window.addEventListener(g, i, !1)
-}(document.querySelector("script[data-psd]"));
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 40 * (clientWidth / 750) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window); //psd 量的大小px除以20 **rem
